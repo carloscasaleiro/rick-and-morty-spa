95dbcc06b1713595e514b3108a0b226bd689d03a
carloscasaleiro <cmcasaleiro@gmail.com>
2023-11-26 14:50:28 +0000

    scrollFix

--- spaRickMorty/js/view/components/banner.js
+++
@@ -1,6 +0,0 @@
-export function banner() {
-  const imageSrc = "./resources/banner.jpeg";
-  const banner = $("<img>").attr({ src: imageSrc, id: "banner" });
-
-  return banner;
-}
--- spaRickMorty/js/view/components/goBack.js
+++
@@ -1,9 +0,0 @@
-export function goBack(classes, content = '', href) {
-    const link = $('<a>');
-
-    if (href) {
-        link.attr({ href });
-    }
-
-    return link.append($('<h1>').text(content).addClass(classes));
-}
